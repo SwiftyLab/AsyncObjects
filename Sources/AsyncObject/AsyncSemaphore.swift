9e69dbb6d6d066ebfe04d920f96154e30a50af96
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2022-07-12 18:54:48 +0530

    doc: add docC calatalog for library

--- Sources/AsyncObject/AsyncSemaphore.swift
+++ Sources/AsyncObject/AsyncSemaphore.swift
@@ -1,12 +1,31 @@
 import Foundation
 import OrderedCollections
 
-actor AsyncSemaphore {
+/// An object that controls access to a resource across multiple task contexts through use of a traditional counting semaphore.
+///
+/// An async semaphore is an efficient implementation of a traditional counting semaphore.
+/// Unlike traditional semaphore, async semaphore suspends current task instead of blocking threads.
+///
+/// You increment a semaphore count by calling the ``signal()`` method
+/// and decrement a semaphore count by calling ``wait()`` method
+/// or its timeout variation ``wait(forNanoseconds:)``.
+public actor AsyncSemaphore {
+    /// The suspended tasks continuation type.
     private typealias Continuation = UnsafeContinuation<Void, Error>
+    /// The continuations stored with an associated key for all the suspended task that are waitig for access to resource.
     private var continuations: OrderedDictionary<UUID, Continuation> = [:]
+    /// Pool size for concurrent resource access.
+    /// Has value provided during initialization incremented by one.
     private var limit: UInt
+    /// Current count of semaphore.
+    /// Can have maximum value upto `limit`.
     private var count: Int
 
+    /// Add continuation with the provided key in `continuations` map.
+    ///
+    /// - Parameters:
+    ///   - continuation: The `continuation` to add.
+    ///   - key: The key in the map.
     private func addContinuation(
         _ continuation: Continuation,
         withKey key: UUID
@@ -14,15 +33,32 @@ actor AsyncSemaphore {
         continuations[key] = continuation
     }
 
+    /// Remove continuation associated with provided key
+    /// from `continuations` map.
+    ///
+    /// - Parameter key: The key in the map.
     private func removeContinuation(withKey key: UUID) {
         continuations.removeValue(forKey: key)
     }
 
+    /// Creates new counting semaphore with an initial value.
+    /// By default, initial value is zero.
+    ///
+    /// Passing zero for the value is useful for when two threads need to reconcile the completion of a particular event.
+    /// Passing a value greater than zero is useful for managing a finite pool of resources, where the pool size is equal to the value.
+    ///
+    /// - Parameter count: The starting value for the semaphore.
+    /// - Returns: The newly created semaphore.
     public init(value count: UInt = 0) {
         self.limit = count + 1
         self.count = Int(limit)
     }
 
+    /// Signals (increments) a semaphore.
+    ///
+    /// Increment the counting semaphore.
+    /// If the previous value was less than zero,
+    /// current task is resumed from suspension.
     public func signal() {
         guard count < limit else { return }
         count += 1
@@ -31,6 +67,10 @@ actor AsyncSemaphore {
         continuation.resume()
     }
 
+    /// Waits for, or decrements, a semaphore.
+    ///
+    /// Decrement the counting semaphore. If the resulting value is less than zero,
+    /// current task is suspended until a signal occurs.
     public func wait() async {
         count -= 1
         if count > 0 { return }
@@ -50,6 +90,13 @@ actor AsyncSemaphore {
         }
     }
 
+    /// Waits for within the duration, or decrements, a semaphore.
+    ///
+    /// Decrement the counting semaphore. If the resulting value is less than zero,
+    /// current task is suspended until a signal occurs or timeout duration completes.
+    ///
+    /// - Parameter duration: The duration in nano seconds to wait until.
+    /// - Returns: The result indicating whether wait completed or timed out.
     @discardableResult
     public func wait(
         forNanoseconds duration: UInt64
