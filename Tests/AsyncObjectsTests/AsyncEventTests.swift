ad42d7246e8dd1b044f81da1d2353838535b0e2e
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2022-07-13 11:18:38 +0530

    test: add async event tests

---
+++ Tests/AsyncObjectsTests/AsyncEventTests.swift
@@ -0,0 +1,76 @@
+import XCTest
+@testable import AsyncObjects
+
+class AsyncEventTests: XCTestCase {
+
+    func checkWait(
+        for event: AsyncEvent,
+        signalIn interval: UInt64 = UInt64(5E9),
+        durationInSeconds seconds: Int = 0
+    ) async throws {
+        Task.detached {
+            try await Task.sleep(nanoseconds: interval)
+            await event.signal()
+        }
+        await checkExecInterval(
+            for: { await event.wait() },
+            durationInSeconds: seconds
+        )
+    }
+
+    func testEventWait() async throws {
+        let event = AsyncEvent(signaledInitially: false)
+        try await checkWait(for: event, durationInSeconds: 5)
+    }
+
+    func testEventLockAndWait() async throws {
+        let event = AsyncEvent()
+        await event.reset()
+        try await checkWait(for: event, durationInSeconds: 5)
+    }
+
+    func testReleasedEventWait() async throws {
+        let event = AsyncEvent()
+        try await checkWait(for: event)
+    }
+
+    func testEventWaitWithTimeout() async throws {
+        let event = AsyncEvent(signaledInitially: false)
+        var result: TaskTimeoutResult = .success
+        await checkExecInterval(
+            for: {
+                result = await event.wait(forNanoseconds: UInt64(4E9))
+            },
+            durationInSeconds: 4
+        )
+        XCTAssertEqual(result, .timedOut)
+    }
+
+    func testEventWaitSuccessWithoutTimeout() async throws {
+        let event = AsyncEvent(signaledInitially: false)
+        var result: TaskTimeoutResult = .timedOut
+        Task.detached {
+            try await Task.sleep(nanoseconds: UInt64(5E9))
+            await event.signal()
+        }
+        await checkExecInterval(
+            for: {
+                result = await event.wait(forNanoseconds: UInt64(10E9))
+            },
+            durationInSeconds: 5
+        )
+        XCTAssertEqual(result, .success)
+    }
+
+    func testReleasedEventWaitSuccessWithoutTimeout() async throws {
+        let event = AsyncEvent()
+        var result: TaskTimeoutResult = .timedOut
+        await checkExecInterval(
+            for: {
+                result = await event.wait(forNanoseconds: UInt64(10E9))
+            },
+            durationInSeconds: 0
+        )
+        XCTAssertEqual(result, .success)
+    }
+}
