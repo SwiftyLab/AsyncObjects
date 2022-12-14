610c8ba60bf6fd9642bafb22b01bc707c2a03f07
soumyamahunt <soumya.mahunt@gmail.com>
2022-12-14 15:24:11 +0530

    wip: add `@_unsafeInheritExecutor` flags

--- Sources/AsyncObjects/Extensions/Task.swift
+++ Sources/AsyncObjects/Extensions/Task.swift
@@ -1,4 +1,32 @@
 public extension Task {
+    #if swift(>=5.7)
+    /// Runs the given operation asynchronously as part of
+    /// a new top-level cancellable task on behalf of the current actor.
+    ///
+    /// Use this function to perform asynchronous work as part of a top-level task
+    /// while being able to automatically cancel top-level task when the current task is cancelled.
+    ///
+    /// - Parameters:
+    ///   - priority: The priority of the task. Pass `nil` to use
+    ///               the priority from `Task.currentPriority`.
+    ///   - operation: The operation to perform.
+    ///
+    /// - Returns: The result from  the given operation, after it completes.
+    /// - Throws: Error from the given operation.
+    @inlinable
+    @discardableResult
+    @_unsafeInheritExecutor
+    static func withCancellableTask(
+        priority: TaskPriority?,
+        operation: @Sendable @escaping () async throws -> Success
+    ) async rethrows -> Success where Failure == Error {
+        let task = Self.init(priority: priority, operation: operation)
+        return try await withTaskCancellationHandler(
+            operation: { try await task.value },
+            onCancel: { task.cancel() }
+        )
+    }
+    #else
     /// Runs the given operation asynchronously as part of
     /// a new top-level cancellable task on behalf of the current actor.
     ///
@@ -24,6 +52,7 @@ public extension Task {
             onCancel: { task.cancel() }
         )
     }
+    #endif
 
     /// Runs the given operation asynchronously as part of
     /// a new top-level cancellable task.
--- Sources/AsyncObjects/Extensions/TaskGroup.swift
+++ Sources/AsyncObjects/Extensions/TaskGroup.swift
@@ -1,3 +1,56 @@
+#if swift(>=5.7)
+public extension TaskGroup {
+    /// Adds a child task to the group and starts the task.
+    ///
+    /// This method adds child task to the group and returns only after the child task is started.
+    ///
+    /// - Parameters:
+    ///   - priority: The priority of the operation task. Omit this parameter or
+    ///               pass `nil` to set the child task’s priority to the priority of the group.
+    ///   - operation: The operation to execute as part of the task group.
+    @inlinable
+    @_unsafeInheritExecutor
+    mutating func addTaskAndStart(
+        priority: TaskPriority? = nil,
+        operation: @escaping @Sendable () async -> ChildTaskResult
+    ) async {
+        typealias C = UnsafeContinuation<Void, Never>
+        await withUnsafeContinuation { (continuation: C) in
+            self.addTask {
+                continuation.resume()
+                return await operation()
+            }
+        }
+    }
+}
+
+public extension ThrowingTaskGroup {
+    /// Adds a child task to the group and starts the task.
+    ///
+    /// This method adds child task to the group and returns only after the child task is started.
+    /// This method doesn’t throw an error, even if the child task does. Instead,
+    /// the corresponding call to `ThrowingTaskGroup.next()` rethrows that error.
+    ///
+    /// - Parameters:
+    ///   - priority: The priority of the operation task. Omit this parameter or
+    ///               pass `nil` to set the child task’s priority to the priority of the group.
+    ///   - operation: The operation to execute as part of the task group.
+    @inlinable
+    @_unsafeInheritExecutor
+    mutating func addTaskAndStart(
+        priority: TaskPriority? = nil,
+        operation: @escaping @Sendable () async throws -> ChildTaskResult
+    ) async {
+        typealias C = UnsafeContinuation<Void, Never>
+        await withUnsafeContinuation { (continuation: C) in
+            self.addTask {
+                continuation.resume()
+                return try await operation()
+            }
+        }
+    }
+}
+#else
 public extension TaskGroup {
     /// Adds a child task to the group and starts the task.
     ///
@@ -47,3 +100,4 @@ public extension ThrowingTaskGroup {
         }
     }
 }
+#endif
